7b2598e9363dff0c5f12ae809e9d9d014e48d90f
Atanas Pashkov <dgaduin@gmail.com>
2020-03-09 20:14:22 +0000

    Update Currency value object

--- src/CheckoutChallenge.Domain/PaymentAggregate/Currency.cs
+++ src/CheckoutChallenge.Domain/PaymentAggregate/Currency.cs
@@ -1,8 +1,25 @@
+using System;
+using System.Collections.Generic;
 using CheckoutChallenge.Domain.SeedWork;
 
 namespace CheckoutChallenge.Domain.PaymentAggregate
 {
     public class Currency : ValueObject
     {
+        public string Value { get; private set; }
+        public Currency(string value)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (value.Length != 3)
+                throw new PaymentAggregateException("Currency code has wrong length");
+
+            Value = value;
+        }
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
     }
 }
