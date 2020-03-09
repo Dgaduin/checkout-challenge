c7c4f9e8687e49708054ed5cc6bfa3f7a8bc09d4
Atanas Pashkov <dgaduin@gmail.com>
2020-03-09 20:12:41 +0000

    Update CardNumber value object and add tests

---
+++ src/CheckoutChallenge.Domain/PaymentAggregate/CardNumber.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using CheckoutChallenge.Domain.SeedWork;
+
+namespace CheckoutChallenge.Domain.PaymentAggregate
+{
+    public class CardNumber : ValueObject
+    {
+        //TODO We can add card type as well
+        public string Value { get; private set; }
+        public CardNumber(string value)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (value.Length < 12 || value.Length > 19)
+                throw new PaymentAggregateException("Card number has incorrect length");
+
+            if (!OnlyNumeric(value))
+                throw new PaymentAggregateException("Card number non numerical symbols in it");
+
+            if (!VerifyLuhnChecksum(value))
+                throw new PaymentAggregateException("Card number failed checksum");
+
+            Value = value;
+        }
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+
+        private static bool VerifyLuhnChecksum(string cardNumber)
+        {
+            int sum = 0;
+            for (int i = cardNumber.Length - 2, j = 1; i >= 0; i--, j++)
+            {
+                // This can be written in a better way probably
+                int a = (int)cardNumber[i] - 48;
+                int b = a * ((j % 2) + 1);
+                int c = (b % 10) + (b / 10);
+                sum += c;
+            }
+            return (sum * 9) % 10 == (int)cardNumber[cardNumber.Length - 1] - 48;
+        }
+
+        private static bool OnlyNumeric(string cardNumber)
+        {
+            Regex r = new Regex("^[Z0-9]*$");
+            return r.IsMatch(cardNumber);
+        }
+    }
+}
---
+++ test/CheckoutChallenge.Domain.Test/CreditCardTests.cs
@@ -0,0 +1,87 @@
+using System;
+using Xunit;
+using CheckoutChallenge.Domain.PaymentAggregate;
+using FluentAssertions;
+
+namespace CheckoutChallenge.Domain.Test
+{
+    public class CreditCardTests
+    {
+        [Fact]
+        public void NumberIsTooShort()
+        {
+            Action a = () => new CardNumber("tooShort");
+            a.Should().ThrowExactly<PaymentAggregateException>();
+        }
+
+        [Fact]
+        public void NumberIsTooLong()
+        {
+            Action a = () => new CardNumber("this card number is going to be way to long");
+            a.Should().ThrowExactly<PaymentAggregateException>();
+        }
+
+        [Fact]
+        public void ContainsNonDigits()
+        {
+            Action a = () => new CardNumber("1234567891011abc");
+            a.Should().ThrowExactly<PaymentAggregateException>();
+        }
+
+        [Theory]
+        [InlineData("38520000023231")]
+        [InlineData("4024007181932131")]
+        [InlineData("4556072498801391")]
+        [InlineData("4929061424591831")]
+        [InlineData("4916550755213031")]
+        [InlineData("4929500402283891")]
+        [InlineData("5108023381750291")]
+        [InlineData("5193605957623551")]
+        [InlineData("5595108497359041")]
+        [InlineData("5536689242785661")]
+        [InlineData("5514271119717971")]
+        [InlineData("6011480225585801")]
+        [InlineData("6011386661160021")]
+        [InlineData("6011495544959551")]
+        [InlineData("6011563271013742")]
+        [InlineData("6011278332089551")]
+        [InlineData("377867071471101")]
+        [InlineData("375415041309221")]
+        [InlineData("344409498386871")]
+        [InlineData("346192973990071")]
+        [InlineData("343692298309051")]
+        public void FailsChecksum(string cardNumber)
+        {
+            Action a = () => new CardNumber(cardNumber);
+            a.Should().ThrowExactly<PaymentAggregateException>();
+        }
+
+        [Theory]
+        [InlineData("38520000023237")]
+        [InlineData("4024007181932137")]
+        [InlineData("4556072498801397")]
+        [InlineData("4929061424591834")]
+        [InlineData("4916550755213039")]
+        [InlineData("4929500402283898")]
+        [InlineData("5108023381750290")]
+        [InlineData("5193605957623558")]
+        [InlineData("5595108497359043")]
+        [InlineData("5536689242785667")]
+        [InlineData("5514271119717978")]
+        [InlineData("6011480225585809")]
+        [InlineData("6011386661160025")]
+        [InlineData("6011495544959550")]
+        [InlineData("6011563271013741")]
+        [InlineData("6011278332089552")]
+        [InlineData("377867071471103")]
+        [InlineData("375415041309227")]
+        [InlineData("344409498386877")]
+        [InlineData("346192973990078")]
+        [InlineData("343692298309055")]
+        public void ValidCardNumbers(string cardNumber)
+        {
+            Action a = () => new CardNumber(cardNumber);
+            a.Should().NotThrow();
+        }
+    }
+}
