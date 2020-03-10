382171af42468e7fa79865df7a12cbbc937d6079
Atanas Pashkov <dgaduin@gmail.com>
2020-03-10 14:12:07 +0000

    Add mock security provider

---
+++ src/CheckoutChallenge.Infrastructure/Security/ISecurityProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CheckoutChallenge.Infrastructure.Security
+{
+    public interface ISecurityProvider
+    {
+        Task<bool> IsKeyValid(string apiKey, Guid merchantId);
+    }
+}
---
+++ src/CheckoutChallenge.Infrastructure/Security/MockSecurityProvider.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CheckoutChallenge.Infrastructure.Security
+{
+    public class MockSecurityProvider : ISecurityProvider
+    {
+        private readonly Dictionary<string, Guid> _merchantKeys = new Dictionary<string, Guid>{
+            {"thisIsKey1",new Guid("B75D838E-E7B0-4B7B-BDA3-1903008085E6")},
+            {"thisIsKey2",new Guid("DA240535-2B38-4376-AD5C-75520BED9BF2")},
+            {"thisIsKey3",new Guid("D4102F94-A06E-48D5-9251-00746C0370F2")},
+            {"thisIsKey4",new Guid("F8876D21-FC27-4BC6-9DB9-5252AF00FD87")}
+        };
+
+        public Task<bool> IsKeyValid(string apiKey, Guid merchantId)
+        {
+            if (_merchantKeys.TryGetValue(apiKey, out Guid id))
+            {
+                if (id == merchantId)
+                    return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
+        }
+    }
+}
